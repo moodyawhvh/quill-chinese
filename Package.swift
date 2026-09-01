c96d0481060eecfa433f402ec81066a25fc97a53
Jeff <moodyawhvh@gmail.com>
2026-09-02 01:37:30 +0800

    添加 Package.swift

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version:6.0
+import PackageDescription
+
+let package = Package(
+    name: "quill",
+    platforms: [.macOS(.v15)],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/FluidInference/FluidAudio.git", from: "0.7.0"),
+    ],
+    targets: [
+        .executableTarget(
+            name: "quill",
+            dependencies: [
+                .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "FluidAudio", package: "FluidAudio"),
+            ],
+            exclude: ["Info.plist"],
+            linkerSettings: [
+                // Embed Info.plist into the binary so TCC can attribute the
+                // system-audio-capture permission to quill itself when it
+                // runs as a LaunchAgent (no .app bundle to carry a plist).
+                .unsafeFlags([
+                    "-Xlinker", "-sectcreate",
+                    "-Xlinker", "__TEXT",
+                    "-Xlinker", "__info_plist",
+                    "-Xlinker", "Sources/quill/Info.plist",
+                ]),
+            ]
+        ),
+    ]
+)
